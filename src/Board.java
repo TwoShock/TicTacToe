a4a28388df42be0b15acf8efbbb602ae05d9c5cb
Mohamad <mohabouharb@gmail.com>
2019-08-06 10:59:18 +0300

    Setup grid and Foundations of AI

--- src/Board.java
+++ src/Board.java
@@ -5,28 +5,68 @@ import java.util.ArrayList;
 public class Board extends GridPane {
     private ArrayList<ArrayList<Cell>> board;//actual gui board
     private ArrayList<ArrayList<Character>> aiBoard;//its a copy of the board used for AI purposes
+    private boolean turn;//0=>x 1=>O
     public final int BOARDSIZE = 3;
     public Board(){
         super();
+        board = new ArrayList<ArrayList<Cell>>();
+        aiBoard = new ArrayList<ArrayList<Character>>();
         initializeAiBoard();
         intializeBoard();
     }
     private void initializeAiBoard(){//handles intialization of aiBoard
         for (int y = 0;y < BOARDSIZE;y++){
             ArrayList<Character> currentList = new ArrayList<Character>();
+            aiBoard.add(currentList);
             for (int x = 0;x < BOARDSIZE;x++){
                 currentList.add(new Character('.'));
             }
         }
     }
+    private void printAIBoard(){
+        for (int y = 0;y < BOARDSIZE;y++) {
+            for (int x = 0; x < BOARDSIZE; x++) {
+                System.out.print(getAiCellAtPos(x,y));
+            }
+            System.out.println();
+        }
+    }
     private void intializeBoard(){//handles adding cells to gridpane
         for (int y = 0;y < BOARDSIZE;y++){
             ArrayList<Cell> currentList = new ArrayList<Cell>();
+            board.add(currentList);
             for (int x = 0;x < BOARDSIZE;x++){
                 Cell currentCell = new Cell();
                 currentList.add(currentCell);
                 add(currentCell,x,y);
+                final int i = x;
+                final int j = y;
+                currentCell.setOnMouseClicked(e->{handleInput(i,j);});
             }
         }
+    }
+    private Cell getCellAtPosition(int x,int y){
+        return board.get(y).get(x);
+    }
+    private void setAICellAtPos(int x,int y,Character c){
+        aiBoard.get(y).set(x,c);
+    }
+    private Character getAiCellAtPos(int x,int y){
+        return aiBoard.get(y).get(x);
+    }
+    private void handleInput(int x,int y){
+
+      if(turn == false){
+          getCellAtPosition(x,y).paint(true);
+          setAICellAtPos(x,y,'X');
+          turn = true;
+      }
+      else{
+          getCellAtPosition(x,y).paint(false);
+          setAICellAtPos(x,y,'O');
+          turn = false;
+      }
+
+        printAIBoard();
     }
 }
--- src/Cell.java
+++ src/Cell.java
@@ -1,4 +1,6 @@
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class Cell extends Button {
     private int size = 100;
@@ -8,4 +10,19 @@ public class Cell extends Button {
         setPrefSize(size,size);
         setFocusTraversable(false);
     }
+    public void paint(boolean x){
+        String imagePath = x ? "assets/x.png":"assets/o.png";
+        Image cellImage = new Image(getClass().getResourceAsStream(imagePath));
+        ImageView imageView = new ImageView(cellImage);
+        imageView.setFitWidth(size/1.5);
+        imageView.setFitHeight(size/1.5);
+        setGraphic(imageView);
+        setDisable(true);
+    }
+    public void reset(){
+        setDisable(false);
+        setGraphic(null);
+
+    }
+
 }
